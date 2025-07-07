ef9dcd39c7351b79089d6b62206b6855a13bc2f6
pedro <pedromssilva16@gmail.com>
2025-07-07 09:54:49 -0300

    condicao ternaria

--- tipos_variaveis/src/TiposVariavel.java
+++ tipos_variaveis/src/TiposVariavel.java
@@ -3,6 +3,13 @@ public class TiposVariavel {
 public static void main(String[] args) {
     System.out.println("hello world,again!");
 System.out.println("is this better VSCODE!?!");
+System.out.println("yeahbaby");
+
+int a,b;
+a=3;
+b=3;
+    String resultado = a==b ? "truezao": "falsao";
+    System.out.println(resultado);
 }
 
 }
